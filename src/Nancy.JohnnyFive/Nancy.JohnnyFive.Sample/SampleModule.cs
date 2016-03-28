495c654838ff1abdba97d28ddd9ebfde0fb3ec9c
Dave Saunders <davesaunders36@gmail.com>
2016-03-28 14:36:13 +0100

    Spike in module demonstrating new way to call api

--- src/Nancy.JohnnyFive/Nancy.JohnnyFive.Sample/SampleModule.cs
+++ src/Nancy.JohnnyFive/Nancy.JohnnyFive.Sample/SampleModule.cs
@@ -30,6 +30,30 @@
 
             Get["/underload"] = _ =>
             {
+
+                // Something like this would be nicer
+                this.CanShortCircuit()
+                    .UsingHandler(new ExceptionHandler<FileNotFoundException>())
+                    .Returning(new LastGoodResponseReturner());
+
+                this.CanShortCircuit()
+                    .UsingHandler(new UnderLoadHandler().ForRequests()
+                    .Returning(new StatusCodeReturner(HttpStatusCode.NoContent))
+                    .ForSeconds(10);
+
+
+
+
+                this.CanShortCircuit()
+                    // Each one of these could get called for each before/after/error and returns the latest
+                    // This would implement an interface that you could derive from to create custom
+                    .UsingHandler(new ExceptionHandler<FileNotFoundException>())
+                    // Something in the before pipeline (or abstracted to the store) returns this when the circuit is closed
+                    // This would also have to be called for above pipelines in order to get last response etc
+                    .Returning(new LastGoodResponseReturner());
+
+
+
                 this.CanShortCircuit(new NoContentUnderLoadCircuit()
                     .WithRequestSampleTimeInSeconds(10)
                     .WithRequestThreshold(5));
