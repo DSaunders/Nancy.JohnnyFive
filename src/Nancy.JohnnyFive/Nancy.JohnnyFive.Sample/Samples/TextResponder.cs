c6b2e8bfb94d08890dbca15a55d295c81d99ebd9
Dave Saunders <davesaunders36@gmail.com>
2016-04-01 14:53:46 +0100

    Add nuspec

--- src/Nancy.JohnnyFive/Nancy.JohnnyFive.Sample/Samples/TextResponder.cs
+++ src/Nancy.JohnnyFive/Nancy.JohnnyFive.Sample/Samples/TextResponder.cs
@@ -1,6 +1,32 @@
 namespace Nancy.JohnnyFive.Sample.Samples
 {
+    using System;
+    using Circuits;
+    using Models;
     using Responders;
+
+    public class FlipFlopCircuit : ICircuit
+    {
+        public CircuitState State { get; set; }
+
+        public void AfterRequest(Response response)
+        {
+        
+        }
+
+        public void BeforeRequest(Request request)
+        {
+            if (State == CircuitState.Normal)
+                State = CircuitState.ShortCircuit;
+            else
+                State = CircuitState.Normal;
+        }
+
+        public void OnError(Exception ex)
+        {
+        
+        }
+    }
 
     public class TextResponder : IResponder
     {
--- src/Nancy.JohnnyFive/Nancy.JohnnyFive/Properties/AssemblyInfo.cs
+++ src/Nancy.JohnnyFive/Nancy.JohnnyFive/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nancy.JohnnyFive")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds fault tollerance to Nancy routes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Nancy.JohnnyFive")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("Nancy.JohnnyFive.Tests")]
